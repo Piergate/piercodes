70cf077edec95ab345b4c712f86fef05149032c4
yamenarahman <yamen.a.abdulrahman@hotmail.com>
2017-12-03 20:27:47 +0200

    add google adsense

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Piercodes - Software and digital marketing agency for high-quality website, mobile and software products." />
     <meta name="keywords" content="Piercodes, pier, codes, software, agency, digital, marketing, websites, design, development, mobile, application, seo, search, engine, optimization, improve, business, developers" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/jquery.lighter.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
@@ -15,11 +15,18 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/jquery.lighter.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-2062767857509083",
+            enable_page_level_ads: true
+        });
+    </script>
 </head>
 <body>
     <div id="app">
-        @yield('content')        
-        
+        @yield('content')
+
     </div>
 </body>
 @stack('scripts')
